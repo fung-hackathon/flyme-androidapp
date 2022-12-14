805634d2c7ed2ba99c71a7d11e66de80c53e7d06
Yourein <rein.you.3.1@gmail.com>
2022-12-15 00:32:52 +0900

    Implement: Modal and its child objects partly

---
+++ app/src/main/java/com/example/fung_p2hacks/HomeScreen/BottomSheet.kt
@@ -0,0 +1,116 @@
+package com.example.fung_p2hacks.HomeScreen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.fung_p2hacks.R
+import com.example.fung_p2hacks.ui.theme.FuNG_p2hacksTheme
+import com.example.fung_p2hacks.ui.theme.PrimaryBlack
+import com.example.fung_p2hacks.ui.theme.PrimaryWhite
+
+@Composable
+fun BottomSheet() {
+    FuNG_p2hacksTheme {
+        Surface(
+            color = MaterialTheme.colors.background
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Divider(
+                    color = PrimaryWhite,
+                    thickness = 5.dp,
+                    modifier = Modifier
+                        .padding(top = 5.dp, bottom = 5.dp, start = 125.dp, end = 125.dp)
+                        .clip(RoundedCornerShape(3.dp))
+                )
+
+                //TODO: 友達を追加
+
+                BottomSheetItem(
+                    text = "友だち申請",
+                    onClick = {},
+                    additionalItem = { PendingFriendsComposable(num = 0) } //TODO: make num variable
+                )
+                BottomSheetItem(
+                    text = "プロフィール設定",
+                    onClick = {}
+                )
+                BottomSheetItem(
+                    text = "ログアウト",
+                    onClick = {}
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun BottomSheetItem(
+    text: String,
+    onClick: () -> Unit,
+    additionalItem: @Composable (() -> Unit) = {}
+) {
+    Surface(
+        onClick = onClick,
+        modifier = Modifier.fillMaxWidth(),
+        color = MaterialTheme.colors.background
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row (verticalAlignment = Alignment.CenterVertically) {
+                Text(text = text, fontWeight = FontWeight.Bold, fontSize = 18.sp)
+                Spacer(modifier = Modifier.padding(5.dp))
+                additionalItem()
+            }
+
+            Image(
+                painter = painterResource(R.drawable.chevron_down),
+                contentDescription = null,
+                modifier = Modifier
+                    .rotate(270F)
+                    .size(20.dp),
+                contentScale = ContentScale.Fit
+            )
+        }
+    }
+}
+
+@Composable
+fun PendingFriendsComposable(num: Int) {
+    Surface(
+        modifier = Modifier
+            .padding(5.dp)
+            .clip(RoundedCornerShape(25.dp)),
+        color = PrimaryWhite,
+        contentColor = PrimaryBlack
+    ) {
+        if (num > 0) {
+            Text(
+                text = "新着${num}人",
+                fontWeight = FontWeight.Bold,
+                fontSize = 10.sp,
+                color = PrimaryBlack,
+                modifier = Modifier.padding(start = 10.dp, end = 10.dp, top = 5.dp, bottom = 5.dp)
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/fung_p2hacks/HomeScreen/HomeActivity.kt
+++ app/src/main/java/com/example/fung_p2hacks/HomeScreen/HomeActivity.kt
@@ -3,21 +3,30 @@ package com.example.fung_p2hacks.HomeScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.fung_p2hacks.R
 import com.example.fung_p2hacks.ui.theme.FuNG_p2hacksTheme
-import com.example.fung_p2hacks.ui.theme.Purple200
+import kotlinx.coroutines.launch
 
 class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FuNG_p2hacksTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background){
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ){
                     Root()
                 }
             }
@@ -25,10 +34,37 @@ class HomeActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun Root() {
+    val sheetState = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+        confirmStateChange = { it != ModalBottomSheetValue.HalfExpanded }
+    )
+    val coroutineScope = rememberCoroutineScope()
+
+    ModalBottomSheetLayout(
+        sheetState = sheetState,
+        sheetContent = { BottomSheet() },
+        modifier = Modifier.fillMaxSize()
+    ) {
+        HomeBodyComposable(
+            modalOpener = {
+                coroutineScope.launch {
+                    if (sheetState.isVisible) sheetState.hide()
+                    else sheetState.show()
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun HomeBodyComposable(
+    modalOpener: () -> Unit
+) {
     Column {
-        TopBarComposable()
+        TopBarComposable(modalOpener)
         TicketComposable()
         FriendsActivityListComposable()
 //        MyHistoryComposable()
