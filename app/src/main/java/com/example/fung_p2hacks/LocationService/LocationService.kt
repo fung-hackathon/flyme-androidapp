9d200a21e6789d0098a9f8ede1c7c22bd7993b50
Yourein <kuuga.2004.31yamazaki@gmail.com>
2022-12-17 19:07:28 +0900

    Implement: MapComposable

---
+++ app/src/main/java/com/example/fung_p2hacks/LocationService/LocationService.kt
@@ -0,0 +1,43 @@
+package com.example.fung_p2hacks.LocationService
+
+import android.os.Looper
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.location.*
+import com.google.android.gms.location.LocationRequest.*
+import com.google.android.gms.maps.model.LatLng
+
+class LocationService(private val fusedLocationClient: FusedLocationProviderClient) {
+    var currentLocation: MutableLiveData<LatLng> = MutableLiveData(LatLng(0.0,0.0))
+
+    private var locationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val locationList = locationResult.locations
+            if (locationList.isNotEmpty()) {
+                val location = locationList.last()
+
+                if (currentLocation.value?.equals(LatLng(location.latitude, location.longitude)) == false) {
+                    currentLocation.value = LatLng(location.latitude, location.longitude)
+
+                    Log.d("Location Data", "Got Location: ${location.latitude}, ${location.longitude}")
+                    Log.d("Location Data", "Live Location: ${currentLocation.value?.latitude}, ${currentLocation.value?.longitude}")
+                }
+            }
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    fun startLocationUpdate() {
+        val locationRequest = LocationRequest().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+    }
+}
--- app/src/main/java/com/example/fung_p2hacks/WalkingScreen/WalkingActivity.kt
+++ app/src/main/java/com/example/fung_p2hacks/WalkingScreen/WalkingActivity.kt
@@ -2,18 +2,27 @@ package com.example.fung_p2hacks.WalkingScreen
 
 import android.Manifest
 import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.example.fung_p2hacks.LocationService.LocationService
+import com.example.fung_p2hacks.LocationService.RequestBackgroundLocationPermission
+import com.example.fung_p2hacks.LocationService.RequestCOARSELocationPermission
+import com.example.fung_p2hacks.LocationService.RequestFineLocationPermission
 import com.example.fung_p2hacks.ui.theme.FuNG_p2hacksTheme
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -21,6 +30,7 @@ import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.tasks.CancellationTokenSource
@@ -33,6 +43,7 @@ class WalkingActivity: ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         val fusedLocationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        val mLocationService = LocationService(fusedLocationClient)
 
         setContent {
             FuNG_p2hacksTheme {
@@ -40,7 +51,22 @@ class WalkingActivity: ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ){
-                    WalkingRootComposable(fusedLocationClient)
+                    val thisContext = LocalContext.current
+                    if (ContextCompat.checkSelfPermission(thisContext, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED){
+                        ActivityCompat.requestPermissions(
+                            thisContext as Activity,
+                            arrayOf(
+                                Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                            ),
+                            1234
+                        )
+                    }
+
+                    mLocationService.startLocationUpdate()
+
+                    WalkingRootComposable(fusedLocationClient, mLocationService)
                 }
             }
         }
@@ -49,44 +75,46 @@ class WalkingActivity: ComponentActivity() {
 
 @Composable
 fun WalkingRootComposable(
-    fusedLocationClient: FusedLocationProviderClient
+    fusedLocationClient: FusedLocationProviderClient,
+    mLocationService: LocationService
 ) {
-    val thisContext = LocalContext.current
     var lat by remember { mutableStateOf(1.35) }
     var lng by remember { mutableStateOf(103.87) }
-    val REQUEST_CODE = 1234
-
+    var pos = LatLng(lat, lng)
     var finished by remember { mutableStateOf(false) }
+    var mapReady by remember { mutableStateOf(false) }
 
-    if (ContextCompat.checkSelfPermission(thisContext, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED){
-        ActivityCompat.requestPermissions(
-            thisContext as Activity,
-            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-            REQUEST_CODE
-        )
-    }
-
-    runBlocking {
-        fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, CancellationTokenSource().token)
-            .addOnSuccessListener { location ->
-                lat = location.latitude
-                lng = location.longitude
-                finished = true
-            }
-    }
+    fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, CancellationTokenSource().token)
+        .addOnSuccessListener { location ->
+            lat = location.latitude
+            lng = location.longitude
+            pos = LatLng(lat, lng)
+            finished = true
+        }
 
     if (finished) {
-        val singapore = LatLng(lat, lng)
         val cameraPositionState = rememberCameraPositionState {
-            position = CameraPosition.fromLatLngZoom(singapore, 15f)
+            position = CameraPosition.fromLatLngZoom(pos, 18f)
         }
+
+        val cLocation = mLocationService.currentLocation.observeAsState()
+
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
-            cameraPositionState = cameraPositionState
+            cameraPositionState = cameraPositionState,
+            onMapLoaded = { mapReady = true }
         ) {
             Marker(
                 state = MarkerState(position = LatLng(lat, lng)),
                 title = "Current Location"
+            )
+        }
+
+        if (mapReady) {
+            cameraPositionState.move(
+                CameraUpdateFactory.newCameraPosition(
+                    CameraPosition.fromLatLngZoom(LatLng(cLocation.value!!.latitude, cLocation.value!!.longitude), 18f)
+                )
             )
         }
     }
