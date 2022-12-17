48bca934cc94309afb7bf1d494473256c67f3c28
Yourein <rein.you.3.1@gmail.com>
2022-12-18 08:05:15 +0900

    Modify: Put "SerializedName" to members

--- app/src/main/java/com/example/fung_p2hacks/API/Model/CredentialModel.kt
+++ app/src/main/java/com/example/fung_p2hacks/API/Model/CredentialModel.kt
@@ -1,5 +1,12 @@
 package com.example.fung_p2hacks.API.Model
 
+import com.google.gson.annotations.SerializedName
+
 data class CredentialModel(
-    val token: String
+    @SerializedName("token") val token: String
+)
+
+data class LogInUser(
+    @SerializedName("userID") val userID: String,
+    @SerializedName("passwd") val passwd: String
 )
